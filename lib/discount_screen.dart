981f41ea90776bb555d7c1d0d04f040c54c50a9f
MD. Rabbi Hossain <fazlyrabbi307@gmail.com>
2025-11-16 22:21:08 +0600

    Discount Screen Added

---
+++ lib/discount_screen.dart
@@ -0,0 +1,286 @@
+import 'package:flutter/material.dart';
+
+class DiscountScreen extends StatefulWidget {
+  const DiscountScreen({super.key});
+
+  @override
+  State<DiscountScreen> createState() => _DiscountScreenState();
+}
+
+class _DiscountScreenState extends State<DiscountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title:Padding(
+          padding: const EdgeInsets.symmetric( vertical: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                      prefixIcon: Icon(Icons.search_outlined),
+                      labelText: "Search Products",labelStyle: TextStyle(fontSize: 16,fontWeight: FontWeight.w500,color: Color(0xffC7C7C7),),
+                      border: OutlineInputBorder(
+                        borderSide:BorderSide(
+                            color: Colors.grey
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.grey)
+                      )
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              Container(
+                height: 45,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: Color(0xffF4A758),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Icon(Icons.tune_outlined),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: 10,
+              itemBuilder: (context, index)=>Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 17.0,vertical: 7),
+                child: Container(
+                  height: 267,
+                  width: double.infinity,
+                  color: Colors.white,
+                  child: Row(
+                    spacing: 20,
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            height: 261,
+                            width: 175,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: Colors.black12,width: 0.5),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.grey.withOpacity(0.1),
+                                      offset: Offset(-1, 5),
+                                      blurRadius: 4
+                                  )
+                                ]
+                            ),
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  flex: 4,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(10),
+                                        topRight: Radius.circular(10),
+                                      ),
+                                      image: DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: AssetImage(
+                                          "assets/Home cart Abeya1.png",
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 3,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(height: 5,),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          "Party Borkha Abaya Koliza",
+                                          style: TextStyle(
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                                        child: Row(
+                                          spacing: 7,
+                                          children: [
+                                            Text(
+                                              "2880",
+                                              style: TextStyle(
+                                                fontSize: 19,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                            Text(
+                                              "3200",
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.w500,
+                                                  decoration: TextDecoration.lineThrough,
+                                                  color: Colors.grey
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(4),
+                                        child: Center(
+                                          child: Container(
+                                            height: 30,
+                                            width: 88,
+                                            decoration: BoxDecoration(
+                                                borderRadius: BorderRadius.circular(5),
+                                                border: Border.all(color: Colors.grey,width: 0.5)
+                                            ),
+                                            child: Center(child: Text("Add To Cart",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 13,color: Colors.black),)),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Positioned(
+                              left: 15,
+                              top: -6,
+                              child: Image(
+                                  height: 35,
+                                  width: 35,
+                                  image: AssetImage("assets/Offer Card.png")))
+                        ],
+                      ),
+                      Stack(
+                        children: [
+                          Container(
+                            height: 261,
+                            width: 175,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(color: Colors.black12,width: 0.5),
+                                boxShadow: [
+                                  BoxShadow(
+                                      color: Colors.grey.withOpacity(0.1),
+                                      offset: Offset(-1, 5),
+                                      blurRadius: 4
+                                  )
+                                ]
+                            ),
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  flex: 4,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.only(
+                                        topLeft: Radius.circular(10),
+                                        topRight: Radius.circular(10),
+                                      ),
+                                      image: DecorationImage(
+                                        fit: BoxFit.fill,
+                                        image: AssetImage(
+                                          "assets/Home cart Abeya2.png",
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  flex: 3,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(height: 5,),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Text(
+                                          "Party Borkha Abaya Koliza",
+                                          style: TextStyle(
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(horizontal: 8),
+                                        child: Row(
+                                          spacing: 7,
+                                          children: [
+                                            Text(
+                                              "2880",
+                                              style: TextStyle(
+                                                fontSize: 19,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                            Text(
+                                              "3200",
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.w500,
+                                                  decoration: TextDecoration.lineThrough,
+                                                  color: Colors.grey
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(4),
+                                        child: Center(
+                                          child: Container(
+                                            height: 30,
+                                            width: 88,
+                                            decoration: BoxDecoration(
+                                                borderRadius: BorderRadius.circular(5),
+                                                border: Border.all(color: Colors.grey,width: 0.5)
+                                            ),
+                                            child: Center(child: Text("Add To Cart",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 13,color: Colors.black),)),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Positioned(
+                              left: 15,
+                              top: -6,
+                              child: Image(
+                                  height: 35,
+                                  width: 35,
+                                  image: AssetImage("assets/Offer Card.png")))
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),),
+          ],
+        ),
+      ),
+    );
+  }
+}
